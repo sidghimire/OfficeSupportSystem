2e46cc4bedd1bdcada9a18140d171c5a46b871eb
Sid Ghimire <siddharthaghimire@gmail.com>
2021-08-15 23:59:39 +0545

    How to enter new line

--- index.c
+++ index.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
-void main()
-{
 
+int main() {
+
+}
+
+void howToInputNewLine(){
+char inputString[128];
+
+   printf("Enter a multi line string\n");
+   scanf("%[^;]s  ", inputString);
+
+   printf("\n%s", inputString);
+
+   return 0;
 }
